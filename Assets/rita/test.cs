530f364ec79f30e5723fd570877fe9ddccb374ab
KOU050223 <kouhei20050223@icloud.com>
2024-06-08 00:59:08 +0900

    [wip]リタファイルを変更
    uozumiファイル内にあるマテリアルを追加
    test.csを書き換え

--- Assets/rita/test.cs
+++ Assets/rita/test.cs
@@ -13,6 +13,9 @@ public class test : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("Fire1");
+        }
     }
 }
